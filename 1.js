d0ab1cadc7a137a742a8a9da8defdcbb5b4b110d
Wobi Nurdiansyah <wobinurdiansyah20@gmail.com>
2024-11-29 10:24:55 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,35 @@
+// let kata = "Rumah kami terbakar";
+// let baru = kata.reverse();
+// console.log(baru)
+
+const kata = "ini kata-kata"
+const baru = kata.split().reverse()
+
+const array1 = ['one', 'two', 'three'];
+const reversed = array1.reverse();
+console.log(reversed);
+// const items = {
+  // kata: "ini ibu budi",
+  // }
+// console.log(items); // [1, 2, 3]
+
+// items.reverse();
+// console.log(items); // [3, 2, 1]
+
+// console.log(kata.revi);
+// function baru() {
+  // let ini = kata.reverse();
+  // return ini;
+// }
+
+// baru()
+// console.log(baru);
+
+// let biodata = {
+  // nama: "naruto",
+  // age: 23,
+  // hooby: "berenang",
+// }
+// new biodata.push() 
+// biodata.riverse()
+// console.log(biodata)
